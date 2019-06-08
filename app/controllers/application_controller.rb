b22d48a5fecd1bec7246133d870b910d2cd12973
Yuji Ishibashi <yu.job96@gmail.com>
2019-06-08 06:49:28 +0000

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+
+	def after_sign_in_path_for(resource)
+    	items_path # ログイン後はアイテム一覧画面に遷移する
+	end
 end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get users_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get users_edit_url
+    assert_response :success
+  end
+
+  test "should get another_address" do
+    get users_another_address_url
+    assert_response :success
+  end
+
+end
