69cee3780fdb9a7108cbf298151bdcd8d5fc884c
石橋勇二 <yuji@ishibashiyuuji-no-MacBook-Air.local>
2019-06-10 02:33:56 +0900

    user削除機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
-  def show
-  	@user = User.find(params[:id])
-  end
-
-  def edit
-  	@user = User.find(params[:id])
-  end
 
   def index
   	@users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -20,20 +21,36 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "User was successfully destroyed."
+      redirect_to users_path
+    else
+      redirect_to users_path
+    end
+  end
+
   def another_address
     @another_address = AnotherAddress.new
   end
+
   def another_address_create
     @another_address = AnotherAddress.new(another_address_params)
     @another_address.user_id = current_user.id
     if @another_address.save
       redirect_to user_path(current_user.id)
+    else
+      render :another_address
     end
   end
 
+  private
+  # ユーザー登録・更新のストロングパラメーター
   def user_params
     params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :postcode, :prefecture, :city, :block, :building)
   end
+  # 別住所登録のストロングパラメーター
   def another_address_params
     params.require(:another_address).permit(:another_last_name, :another_first_name, :another_last_name_kana, :another_first_name_kana, :another_postcode, :another_prefecture, :another_city, :another_block, :another_building)
   end
