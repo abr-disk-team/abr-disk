ad538afcc2e73fb74f9f84f35c83563b2da40cde
石橋勇二 <yuji@ishibashiyuuji-no-MacBook-Air.local>
2019-06-10 02:01:31 +0900

    another_address追加機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,13 +22,19 @@ class UsersController < ApplicationController
 
   def another_address
     @another_address = AnotherAddress.new
-    
   end
   def another_address_create
-    @another_address
+    @another_address = AnotherAddress.new(another_address_params)
+    @another_address.user_id = current_user.id
+    if @another_address.save
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :postcode, :prefecture, :city, :block, :building)
+  end
+  def another_address_params
+    params.require(:another_address).permit(:another_last_name, :another_first_name, :another_last_name_kana, :another_first_name_kana, :another_postcode, :another_prefecture, :another_city, :another_block, :another_building)
   end
 end
