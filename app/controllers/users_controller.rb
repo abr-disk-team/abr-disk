409b1241bcfae6471946927837b54331fc2541ba
石橋勇二 <yuji@ishibashiyuuji-no-MacBook-Air.local>
2019-06-10 00:47:26 +0900

    ヘッダーの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,26 @@ class UsersController < ApplicationController
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
 
   def index
   	@users = User.all
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user == current_user && @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
   def another_address
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :postcode, :prefecture, :city, :block, :building)
   end
 end
