94e7b18aefdf189892083fc860732e88321bf17c
Yuji-Ishibahi <yu.job96@gmail.com>
2019-06-18 18:33:58 +0900

    create favorite table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_094936) do
+ActiveRecord::Schema.define(version: 2019_06_18_091424) do
 
   create_table "another_addresses", force: :cascade do |t|
     t.integer "user_id"
@@ -64,6 +64,13 @@ ActiveRecord::Schema.define(version: 2019_06_14_094936) do
   create_table "discs", force: :cascade do |t|
     t.integer "item_id"
     t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
