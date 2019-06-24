fec4ef5b05896d13d34c59bb863b0703d15dccb8
Yuji-Ishibahi <yu.job96@gmail.com>
2019-06-24 13:38:30 +0900

    add order index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_051414) do
+ActiveRecord::Schema.define(version: 2019_06_23_101805) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2019_06_23_051414) do
 
   create_table "artists", force: :cascade do |t|
     t.string "artist"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
